90de7d4ab4182727f56b175a03df30370f8ca820
baligaandris <baligaandris@gmail.com>
2017-02-08 13:50:41 +0000

    messed around in my test scene

---
+++ Assets/EnemyNavScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyNavScript : MonoBehaviour {
+
+    public Transform Goal;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<NavMeshAgent>().SetDestination(Goal.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
