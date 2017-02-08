0e77edb663ec9d5a185e27e909a65a4b9f56bef5
baligaandris <baligaandris@gmail.com>
2017-02-08 16:08:58 +0000

    tower targeting works

--- Assets/Scripts/EnemyHealthScript.cs
+++ Assets/Scripts/EnemyHealthScript.cs
@@ -14,6 +14,7 @@ public class EnemyHealthScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         if (health <= 0) {
+
             Destroy(gameObject);
         }
 	}
--- Assets/Scripts/TowerShootsScript.cs
+++ Assets/Scripts/TowerShootsScript.cs
@@ -1,39 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
+
+public class Enemy
+{
+    public GameObject EnemyObject;
+    public int health;
+    public Enemy(GameObject enemyIn, int hp)
+    {
+        EnemyObject = enemyIn;
+        health = hp;
+    }
+}
 
 public class TowerShootsScript : MonoBehaviour {
 
-    public GameObject target;
+    
+
+    public List<Enemy> targets;
     public int damage = 5;
     private float shootCoolDown = 0;
     public float fireRate = 0.3f;
 
+
+
 	// Use this for initialization
 	void Start () {
-	
+        targets = new List<Enemy>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         shootCoolDown += Time.deltaTime;
-        if (target != null  && shootCoolDown >=fireRate){
-            target.GetComponent<EnemyHealthScript>().TakeDamage(damage);
+        if (targets.Count != 0  && shootCoolDown >=fireRate){
+
+            while (targets[0].EnemyObject == null) {
+                    targets.Remove(targets[0]);
+                if (targets.Count == 0) {
+                    return;
+                }
+            }
+
+            targets[0].EnemyObject.GetComponent<EnemyHealthScript>().TakeDamage(damage);
+            targets[0].health = targets[0].EnemyObject.GetComponent<EnemyHealthScript>().health;
+            if (targets[0].health <= 0) {
+                targets.Remove(targets[0]);
+            }
             shootCoolDown = 0;
         }
 	}
 
-    void OnTriggerStay(Collider other) {
+    void OnTriggerEnter(Collider other) {
         Debug.Log("enemy in range");
-        if (other.gameObject.tag == "Enemy" && target == null)
+        if (other.gameObject.tag == "Enemy")
         {
-            target = other.gameObject;
+            Enemy newEnemy = new Enemy(other.gameObject, other.gameObject.GetComponent<EnemyHealthScript>().health);
+            targets.Add(newEnemy);
         }
     }
 
     void OnTriggerExit(Collider other) {
-        if (other.gameObject == target) {
-            target = null;
+
+        for (int i = 0; i < targets.Count; i++) {
+            if (targets[i].EnemyObject == other.gameObject) {
+                targets.Remove(targets[i]);
+            }
         }
+
     }
+
 
 }
