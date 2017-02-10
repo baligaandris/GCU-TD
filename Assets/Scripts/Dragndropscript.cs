f863c1ccea19d619adb63c830b03aa7f43da33ac
DragostinH <dragostin.hristov@gmail.com>
2017-02-10 23:47:34 +0000

    Added a drag n drop button for 1 turret
    Added a quad and terrain to work with the Drandrop script.

---
+++ Assets/Scripts/Dragndropscript.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Dragndropscript : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public GameObject tower;
+    GameObject hoverTower;
+
+    // Use this for initialization
+    void Start()
+    {
+        hoverTower = Instantiate(tower);
+        hoverTower.SetActive(false);
+        RemoveFunctionFromPrefab();
+    }
+
+    void RemoveFunctionFromPrefab()
+    {
+        Component[] components = hoverTower.GetComponentsInChildren<TowerShootsScript>();
+        foreach (Component component in components)
+        {
+            Destroy(component);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
+    {
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        RaycastHit[] hits;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        hits = Physics.RaycastAll(ray, 50f);
+        if (hits != null && hits.Length > 0)
+        {
+            int terrainCollderQuadIndex = GetTerrainColliderQuadIndex(hits);
+            if (terrainCollderQuadIndex != -1)
+            {
+                hoverTower.transform.position = hits[terrainCollderQuadIndex].point;
+                hoverTower.SetActive(true);
+                // Debug.Log (hits [terrainCollderQuadIndex].point);
+            }
+            else
+            {
+                hoverTower.SetActive(false);
+            }
+        }
+    }
+
+    int GetTerrainColliderQuadIndex(RaycastHit[] hits)
+    {
+        for (int i = 0; i < hits.Length; i++)
+        {
+            if (hits[i].collider.gameObject.name.Equals("TerrainColliderQuad"))
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        // If the prefab instance is active after dragging stopped, it means
+        // it's in the arena so (for now), just drop it in.
+        if (hoverTower.activeSelf)
+        {
+            Instantiate(tower, hoverTower.transform.position, Quaternion.identity);
+        }
+
+        // Then set it to inactive ready for the next drag!
+        hoverTower.SetActive(false);
+    }
+}
