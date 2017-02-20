7dca14c6dd674d50fd7bc49c93524b24825226eb
DragostinH <dragostin.hristov@gmail.com>
2017-02-20 20:47:46 +0000

    Healthbar added to enemies
    Added 2 scripts
    1x That keeps the health bar pointing at the main camera each frame
    1x That Changes the health bar after an enemy takes damage
    Added comments to both scripts
    
    Note: It says 98 changes since I had to change copy/paste the whole
    project. Apologies.

--- Assets/Scripts/EnemyHealthScript.cs
+++ Assets/Scripts/EnemyHealthScript.cs
@@ -5,18 +5,22 @@ public class EnemyHealthScript : MonoBehaviour {
 
     public int usacValue = 10; //this is the money value of this enemy
     private GameDataScript gameData; // reference to the gamedatascript, the one and only
-    public int health = 100; //how much health the enemy has
+    public int health = 100; //how much health the enemy starts with
+    public float currentHealth; // how much health the enemy has
     public int damage = 1; //how much damage they deal to the Uni, if they reach it
     public bool runningAway = false;
 
-	// Use this for initialization
-	void Start () {
+    public delegate void HealthChanged(float currentHealth, float health);
+    public event HealthChanged OnHealthChanged;
+    // Use this for initialization
+    void Start () {
         //here we get the gamedatascript, to reference later
         gameData = GameObject.FindGameObjectWithTag("GameData").GetComponent<GameDataScript>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        currentHealth = health;
+    }
+
+    // Update is called once per frame
+    void Update () {
         //if their health reaches 0, they add their value to the money we have, and they get destroyed
         if (health <= 0) {
             gameData.ChangeUsac(usacValue);
@@ -28,5 +32,9 @@ public class EnemyHealthScript : MonoBehaviour {
     //this method is called by the towers to deal damage to the enemy
     public void TakeDamage(int damage) {
         health -= damage;
+        if (OnHealthChanged != null)
+        {
+            OnHealthChanged(currentHealth, health);
+        }
     }
 }
---
+++ Assets/Scripts/WaypointScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointScript : MonoBehaviour {
+
+    public GameObject nextWayPoint; //points to the next waypoint
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+}
