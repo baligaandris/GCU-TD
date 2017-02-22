c13f4dfae10dd1edd9106541923fa880c6b38931
liamfergie <lfergu209@caledonian.ac.uk>
2017-02-22 22:53:51 +0000

    Added pause menu background
    Pauses with the "P" key, no functionality yet

---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour {
+
+	public Transform pauseMenu;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.P)) {
+			if (pauseMenu.gameObject.activeInHierarchy == false) {
+				pauseMenu.gameObject.SetActive (true);
+				Time.timeScale = 0;
+			} else {
+				pauseMenu.gameObject.SetActive (false);
+				Time.timeScale = 1;
+			}
+		}
+	}
+}
